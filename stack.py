29e0a7c2bcfd77a30a25c9c45705e6ba77171d3c
Raheem Azeez Abiodun <raheemazeez4@gmail.com>
2019-06-14 12:39:46 +0100

    wrote documentation

--- stack.py
+++ stack.py
@@ -1,13 +1,37 @@
+"""
+A basic implementation of the Stack data structure.
+
+Usage:
+    stack = Stack()
+    stack.push(data) # data can be anything e.g String, Int, list
+    stack.peek()
+    stack.size()
+    stack.pop()
+    stack.size()
+"""
+
+
 class Stack:
     def __init__(self):
-        self.count = 0  # holds stack
+        """Constructor."""
+        self.count = 0
         self.stack = {}
 
     def push(self, value):
+        """
+        Push a value to the top of the stack
+
+        :params: value
+        """
         self.stack[self.count] = value
         self.count += 1
 
     def pop(self):
+        """
+        Pop the last item was added to the stack
+
+        :return : the last value of the stack
+        """
         if self.count < 1:
             return None
         else:
@@ -17,12 +41,19 @@ class Stack:
             return popped_item
 
     def peek(self):
+        """Return the last item added."""
         return self.stack[self.count - 1]
 
-    def get_length(self):
+    def is_empty(self):
+        """Return a boolean if stack is empty."""
+        return not self.stack
+
+    def size(self):
+        """Return the size of the stack."""
         return self.count
 
     def get_stack(self):
+        """Return the size of the stack."""
         return self.stack
 
     def __str__(self):
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -6,21 +6,34 @@ class TestStack(TestCase):
     def setUp(self):
         self.stack_obj = Stack()
     
-    def test_empty_stack(self):
-        self.assertDictEqual(self.stack_obj.get_stack(), {})
-
-    def test_stack_method(self):
-        self.stack_obj.push('Abbey')
-        self.stack_obj.push('Biodun')
-
-        self.assertEqual(self.stack_obj.get_length(), 2)
-        self.assertDictEqual(self.stack_obj.get_stack(), {0: 'Abbey', 1: 'Biodun'})
+    def test_push_method(self):
+        self.stack_obj.push('book1')
+        self.stack_obj.push('book2')
         
-        self.stack_obj.pop()
-        self.assertEqual(self.stack_obj.get_length(), 1)
-        self.assertDictEqual(self.stack_obj.get_stack(), {0: 'Abbey'})
+        self.assertDictEqual(self.stack_obj.get_stack(), {
+            0: 'book1',
+            1: 'book2'
+        })
 
-        self.stack_obj.pop()
-        self.stack_obj.pop()
+    def test_pop_method(self):
+        self.stack_obj.push('Nigeria')
+        self.assertEqual(self.stack_obj.pop(), 'Nigeria')
         self.assertDictEqual(self.stack_obj.get_stack(), {})
-        self.assertEqual(self.stack_obj.get_length(), 0)
+        self.assertIsNone(self.stack_obj.pop())
+
+    def test_peek(self):
+        self.stack_obj.push('USA')
+        self.stack_obj.push('Russia')
+
+        self.assertEqual(self.stack_obj.peek(), 'Russia')
+
+    def test_is_empty_method(self):
+        self.assertTrue(self.stack_obj.is_empty())
+
+        self.stack_obj.push('Yankee')
+        self.assertFalse(self.stack_obj.is_empty())
+
+    def test_get_size_method(self):
+        self.assertEqual(self.stack_obj.size(), 0)
+        self.stack_obj.push('New York')
+        self.assertEqual(self.stack_obj.size(), 1)
